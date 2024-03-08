fbc5c52da42e232fd826f4af1b8822a20a2442a9
xkayah <1806986098@qq.com>
2024-03-08 13:23:05 +0800

    [update] 启动日志

--- ucenter/src/main/java/com/mnus/train12306/config/UCenterApplication.java
+++ ucenter/src/main/java/com/mnus/train12306/config/UCenterApplication.java
@@ -1,8 +1,11 @@
 package com.mnus.train12306.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 /**
  * @author: <a href="https://github.com/xkayah">xkayah</a>
@@ -11,7 +14,12 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.mnus")
 public class UCenterApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(UCenterApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(UCenterApplication.class);
+
+        Environment env = SpringApplication.run(UCenterApplication.class).getEnvironment();
+        LOG.info("ucenter start! test url:\thttp://127.0.0.1:{}{}/hello",
+                env.getProperty("server.port"),
+                env.getProperty("server.servlet.context-path"));
     }
 }
