9dec743740c6907ec96f944e6d1d385cef742f90
xkayah <1806986098@qq.com>
2024-03-09 20:11:46 +0800

    [add] 增加登录注册页，未对接后端接口

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -10,10 +10,15 @@ const routes = [
   {
     path: '/about',
     name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/the-login.vue')
   }
 ]
 
---
+++ web/src/views/the-login.vue
@@ -0,0 +1,96 @@
+<template>
+    <a-row class="login">
+        <a-divider class="logo-body">
+            <car-outlined two-tone-color="#eb2f96" width="50px"/>
+            <br/>
+            <h1 style="text-align: center">
+                Sign in to Mnus12306
+            </h1>
+        </a-divider>
+        <a-col :span="8" :offset="8" class="login-form-body">
+            <a-form
+                    :model="loginForm"
+                    name="basic"
+                    autocomplete="off"
+            >
+                <h3>Username or mobile</h3>
+                <a-form-item
+                        name="mobile"
+                        :rules="[{ required: true, message: '请输入手机号!' }]"
+                >
+                    <a-input v-model:value="loginForm.mobile">
+                        <template #suffix>
+                            <a-tooltip title="Extra information">
+                                <info-circle-outlined style="color: rgba(0, 0, 0, 0.45)"/>
+                            </a-tooltip>
+                        </template>
+                    </a-input>
+                </a-form-item>
+
+                <h3>Code</h3>
+                <a-form-item
+                        name="code"
+                        :rules="[{ required: true, message: '请输入验证码!' }]"
+                >
+                    <a-input v-model="loginForm.code">
+                        <template #addonAfter>
+                            <a click="sendCode">send code</a>
+                        </template>
+                    </a-input>
+
+                </a-form-item>
+
+                <a-form-item :wrapper-col="{ offset: 8, span: 8 }">
+                    <a-button type="primary" html-type="submit">Sign in</a-button>
+                </a-form-item>
+            </a-form>
+        </a-col>
+    </a-row>
+</template>
+
+<script>
+import {defineComponent, reactive} from 'vue';
+
+export default defineComponent({
+    name: "the-login",
+    setup() {
+        const loginForm = reactive({
+            mobile: '',
+            code: '',
+        });
+        const onFinish = values => {
+            console.log('Success:', values);
+        };
+        const onFinishFailed = errorInfo => {
+            console.log('Failed:', errorInfo);
+        };
+        const sendCode = values => {
+            console.log('Success:', values);
+        };
+        return {
+            loginForm,
+            onFinish,
+            onFinishFailed,
+            sendCode,
+        };
+    },
+});
+</script>
+<style>
+.login-form-body {
+    margin-top: 10px;
+    padding: 30px 30px 20px;
+    border: 2px solid rgb(216, 222, 228);
+    border-radius: 10px;
+    background-color: rgb(246, 248, 250);
+}
+
+.login-form-body h3 {
+    text-align: left;
+}
+
+.logo-body{
+
+}
+
+</style>
