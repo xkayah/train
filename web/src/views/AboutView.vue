ab4e29ff030b27aabdf9bfe6dd5046fbb617b41b
xkayah <1806986098@qq.com>
2024-03-09 17:18:35 +0800

    [add] 引入 Ant Design Vue

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <a-button>button</a-button>
+        <smile-two-tone />
+    </div>
 </template>
