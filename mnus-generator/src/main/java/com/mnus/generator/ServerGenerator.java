a5f6f9a9ae9a109c7fe0c3f9318a66f4250f4c34
xkayah <1806986098@qq.com>
2024-03-13 21:07:03 +0800

    [add] 集成dom4j；读pom.xml文件，取当前持久层文件路径

---
+++ mnus-generator/src/main/java/com/mnus/generator/ServerGenerator.java
@@ -0,0 +1,34 @@
+package com.mnus.generator;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+
+import java.util.HashMap;
+
+/**
+ * @author: <a href="https://github.com/xkayah">xkayah</a>
+ * @date: 2024/3/13 20:49:54
+ */
+public class ServerGenerator {
+    public static final String POM_PATH = "mnus-generator\\pom.xml";
+    public static final String TO_PATH = "mnus-generator\\pom.xml";
+    public static final String MODULE_NAME = "mnus-generator\\pom.xml";
+    public static final String YY = "mnus-generator\\pom.xml";
+    public static final String XX = "mnus-generator\\pom.xml";
+
+    public static void main(String[] args) throws Exception {
+        String generatorCfgPath = getGeneratorCfgPath();
+    }
+
+    private static String getGeneratorCfgPath() throws DocumentException {
+        SAXReader saxReader = new SAXReader();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(POM_PATH);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        return node.getText();
+    }
+}
