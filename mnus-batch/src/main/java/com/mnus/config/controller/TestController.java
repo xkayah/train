465a2875cff043e807eafa36834d1869ea9168ff
xkayah <1806986098@qq.com>
2024-03-15 20:19:01 +0800

    [add] 增加batch模块

---
+++ mnus-batch/src/main/java/com/mnus/config/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.mnus.config.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: <a href="https://github.com/xkayah">xkayah</a>
+ * @date: 2024/3/8 12:12:56
+ */
+@RestController
+public class TestController {
+    @Resource
+    private Environment env;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return String.format("Hello %s Service!", env.getProperty("spring.application.name"));
+    }
+}
