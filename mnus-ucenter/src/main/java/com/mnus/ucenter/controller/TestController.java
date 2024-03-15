d0d95cc3e3567849fd7c5ce31ab215ae04c273f1
xkayah <1806986098@qq.com>
2024-03-15 20:12:37 +0800

    [update]

--- mnus-ucenter/src/main/java/com/mnus/ucenter/controller/TestController.java
+++ mnus-ucenter/src/main/java/com/mnus/ucenter/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.mnus.ucenter.controller;
 
+import jakarta.annotation.Resource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    @Resource
+    private Environment env;
+
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello UCenter Server!";
+    public String hello() {
+        return String.format("Hello %s Service!", env.getProperty("spring.application.name"));
     }
 }
