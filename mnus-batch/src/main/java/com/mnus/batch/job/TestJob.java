3a2a1717a64bbbfd94f10747b088dedfb82109ce
xkayah <1806986098@qq.com>
2024-03-16 11:32:46 +0800

    [add] 在执行任务之前放入tid，解决日志tid丢失的问题。因为异步线程没经过拦截器，自然也就没有tid

--- mnus-batch/src/main/java/com/mnus/batch/job/TestJob.java
+++ mnus-batch/src/main/java/com/mnus/batch/job/TestJob.java
@@ -1,10 +1,16 @@
 package com.mnus.batch.job;
 
 import cn.hutool.core.date.DateTime;
+import com.mnus.common.constance.MDCKey;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.UUID;
 
 /**
  * @author: <a href="https://github.com/xkayah">xkayah</a>
@@ -12,9 +18,14 @@ import org.quartz.JobExecutionException;
  */
 @DisallowConcurrentExecution
 public class TestJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestJob.class);
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        // todo 自定义线程池，在每次启动线程时添加 tid
+        MDC.put(MDCKey.TID, UUID.randomUUID().toString());
         System.out.println("QuartzTestJob start..." + DateTime.now());
+        LOG.info("do test job...");
         System.out.println("QuartzTestJob end  ..." + DateTime.now());
     }
 }
