c2a5619f8e7d8e71fb659e72add7741ebf8ea048
xkayah <1806986098@qq.com>
2024-03-16 11:50:26 +0800

    [add] 集成openfeign，实现服务间调用

--- mnus-batch/src/main/java/com/mnus/batch/controller/TestController.java
+++ mnus-batch/src/main/java/com/mnus/batch/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.mnus.batch.controller;
 
+import com.mnus.batch.feign.TestFeign;
 import jakarta.annotation.Resource;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +15,12 @@ public class TestController {
     @Resource
     private Environment env;
 
+    @Resource
+    private TestFeign testFeign;
+
     @GetMapping("/hello")
     public String hello() {
+        System.out.printf("[feign]:%s\n", testFeign.hello());
         return String.format("Hello %s Service!", env.getProperty("spring.application.name"));
     }
 }
---
+++ mnus-batch/src/main/java/com/mnus/batch/feign/TestFeign.java
@@ -0,0 +1,15 @@
+package com.mnus.batch.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author: <a href="https://github.com/xkayah">xkayah</a>
+ * @date: 2024/3/16 11:38:52
+ */
+@FeignClient(name = "business", url = "http://127.0.0.1:8101/business")
+public interface TestFeign {
+
+    @GetMapping("/hello")
+    String hello();
+}
