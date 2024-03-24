eb66580895321aab465aa1b48526a08a3b6adade
xkayah <1806986098@qq.com>
2024-03-24 16:23:28 +0800

    [add] 配置 Gateway Feign Nacos 支持应用名路由转发

--- mnus-batch/src/main/java/com/mnus/batch/feign/BusinessFeign.java
+++ mnus-batch/src/main/java/com/mnus/batch/feign/BusinessFeign.java
@@ -12,13 +12,14 @@ import java.util.Date;
  * @author: <a href="https://github.com/xkayah">xkayah</a>
  * @date: 2024/3/16 11:38:52
  */
-@FeignClient(name = "business", url = "http://127.0.0.1:8101/business")
+@FeignClient("business")
+// @FeignClient(name = "business", url = "http://127.0.0.1:8101/business")
 public interface BusinessFeign {
 
-    @GetMapping("/hello")
+    @GetMapping("/business/hello")
     String hello();
 
-    @GetMapping("/admin/daily-train/gen-daily/{date}")
+    @GetMapping("/business/admin/daily-train/gen-daily/{date}")
     CommonResp<Object> genDailyTrain(
             @PathVariable
             @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
--- mnus-business/src/main/java/com/mnus/business/feign/UcenterFeign.java
+++ mnus-business/src/main/java/com/mnus/business/feign/UcenterFeign.java
@@ -11,11 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @author: <a href="https://github.com/xkayah">xkayah</a>
  * @date: 2024/3/24 9:24:40
  */
-@FeignClient(name = "ucenter", url = "http://127.0.0.1:8001/ucenter")
+@FeignClient("ucenter")
+// @FeignClient(name = "ucenter", url = "http://127.0.0.1:8001/ucenter")
 public interface UcenterFeign {
 
 
-    @PostMapping("/admin/ticket/insert")
+    @PostMapping("/ucenter/admin/ticket/insert")
     CommonResp<Object> insert(@Valid @RequestBody TicketInsertReq req);
 
 }
